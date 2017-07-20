95388d02cd22465acec7e94d5168525d786776ef
John Furtado <jf8846@gmail.com>
2017-07-20 19:58:20 -0400

    able to switch between Farenheit and Celsius

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 
 $(document).ready(function() {
    getLocation();
+   $('#unit').click(function() {
+   		convert();
+   });
 });
 
 function getWeather(position) {
@@ -12,20 +15,36 @@ function getWeather(position) {
         	console.log(response);
         	$('#city').text(response.name + ",");
         	$('#country').text(response.sys.country);
-            $('#temp').text(response.main.temp);
-            $('#units').html("&#176F")
+            $('#temp').text(Math.round(response.main.temp));
+            $('#degree').html("&#176");
+            $('#unit').text("F");
             $('#descr').text(response.weather[0].description); 
             var icon = response.weather[0].icon; 
             var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
             $('#icon').html("<img src='" + iconUrl  + "'>");
         }
     });
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
 }
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getWeather);
     } 
+}
+
+function convert() {
+    var x;
+    var unit = $('#unit').text();
+    var temp = $('#temp').text();
+    console.log(unit);
+    if (unit === "F") {
+        x = (temp - 32) * 5 / 9;
+        $('#temp').text(Math.round(x));
+        $('#unit').text("C");
+    } 
+    if (unit === "C") {
+        x = temp * 9 / 5 + 32;
+        $('#temp').text(Math.round(x));
+        $('#unit').text("F");
+    }
 }
