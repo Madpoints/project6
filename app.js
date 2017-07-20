0e3a4e1c35e850a0435263f87fc5ba2984417014
John Furtado <jf8846@gmail.com>
2017-07-20 18:58:57 -0400

    uses data from ajax call to provide content for page

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 /*global $*/
-var long;
 
-var lang;
 
 $(document).ready(function() {
-   getWeather(long, lang); 
+   getLocation();
 });
 
-function getWeather(long, lang) {
+function getWeather(position) {
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=Bristol,US&APPID=d97c4820eeb5a1aad58317e076d46d45",
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=imperial&APPID=d97c4820eeb5a1aad58317e076d46d45',
         success: function(response) {
-            console.log(response);    
+        	console.log(response);
+        	$('#city').text(response.name + ",");
+        	$('#country').text(response.sys.country);
+            $('#temp').text(response.main.temp);
+            $('#units').html("&#176F")
+            $('#descr').text(response.weather[0].description); 
+            var icon = response.weather[0].icon; 
+            var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+            $('#icon').html("<img src='" + iconUrl  + "'>");
         }
     });
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather);
+    } 
 }
